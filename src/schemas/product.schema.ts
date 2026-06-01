aa1da766b1514c2b18e75033e54ef65da35b1c37
otaviobonini <otgbonini@gmail.com>
2026-06-01 17:17:38 -0300

    fix: fixes in integration

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -6,7 +6,14 @@ export const CreateProductSchema = z.object({
   stock: z.coerce.number().int().positive(),
 });
 
-export const EditProductSchema = CreateProductSchema.partial();
+export const EditProductSchema = z.object({
+  productName: z.string().optional(),
+  productPrice: z.coerce.number().positive().optional(),
+  productDescription: z.string().optional(),
+  stock: z.coerce.number().int().min(0).optional(),
+  isFeatured: z.boolean().optional(),
+  categoryId: z.coerce.number().int().positive().optional().nullable(),
+});
 
 export const ProductIdParamSchema = z.object({
   productId: z
@@ -30,5 +37,6 @@ export const GetProductsQuerySchema = z.object({
     .default(0),
 });
 
+export type EditProductInput = z.infer<typeof EditProductSchema>;
 export type CreateProductInput = z.infer<typeof CreateProductSchema>;
 export type GetProductsQueryInput = z.infer<typeof GetProductsQuerySchema>;
