6df04cd2125c7f6950311f14ca849b420205e1ea
otaviobonini <otgbonini@gmail.com>
2026-06-01 17:13:00 -0300

    feat: creates categories module

---
+++ src/modules/categories/CategoriesService.ts
@@ -0,0 +1,40 @@
+import { ICategoryRepository } from "../../types/ICategoriesRepository.js";
+import { AppError } from "../../common/AppError.js";
+
+export class CategoriesService {
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  async createCategory(name: string) {
+    const exists = await this.categoryRepository.findByName(name);
+    if (exists) throw new AppError(409, "Category already exists");
+    return this.categoryRepository.createCategory(name);
+  }
+
+  async deleteCategory(categoryId: number) {
+    const category = await this.categoryRepository.findById(categoryId);
+    if (!category) throw new AppError(404, "Category not found");
+    return this.categoryRepository.deleteCategory(categoryId);
+  }
+
+  async findAll() {
+    return this.categoryRepository.findAll();
+  }
+
+  async getProductsByCategory(
+    categoryId: number,
+    offset: number,
+    limit: number,
+  ) {
+    const category = await this.categoryRepository.findById(categoryId);
+    if (!category) throw new AppError(404, "Category not found");
+    return this.categoryRepository.getProductsByCategory(
+      categoryId,
+      offset,
+      limit,
+    );
+  }
+
+  async getFeaturedProducts(offset: number, limit: number) {
+    return this.categoryRepository.getFeaturedProducts(offset, limit);
+  }
+}
